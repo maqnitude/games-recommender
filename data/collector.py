71a506efc94e8e5b19bb5a02d637a7354eb85a0f
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-01-05 22:35:17 +0700

    add more columns and more batches of reviews

--- data/collector.py
+++ data/collector.py
@@ -12,31 +12,58 @@ games_csv_path = os.path.join(BASE_DIR, 'collected', config.GAMES_CSV)
 users_games_csv_path = os.path.join(BASE_DIR, 'collected', config.USERS_GAMES_CSV)
 
 GAMES_COLUMNS = [
-    'app_id', 'name', 'required_age', 'is_free',
-    'categories', 'genres',
-    'short_description',
-    'developers', 'publishers',
+    # Details
+    'app_id',
+    'name',
+    'required_age',
+    'is_free',
+    'developers',
+    'publishers',
+    'platforms_windows',
+    'platforms_mac',
+    'platforms_linux',
+    'metacritic',
+    'categories',
+    'genres', 
+    'recommendations',
+    'coming_soon',
     'release_date',
-    'metacritic_score',
-    'review_score', 'review_score_desc', 'total_positive', 'total_negative', 'total_reivews',
+
+    # Reviews
+    'review_score',
+    'review_score_desc',
+    'total_positive',
+    'total_negative',
+    'total_reivews',
 ]
 
 USERS_GAMES_COLUMNS = [
-    'steam_id', 'app_id',
-    'voted_up', 'votes_up', 'votes_funny',
-    'playtime_forever', 'playtime_last_two_weeks', 'playtime_at_review',
-    'timestamp_created', 'timestamp_updated'
+    'steam_id',
+    'app_id',
+    'num_games_owned',
+    'num_reviews',
+    'playtime_forever',
+    'playtime_last_two_weeks',
+    'playtime_at_review',
+    'last_played',
+    'timestamp_created',
+    'timestamp_updated'
+    'voted_up',
+    'votes_up',
+    'votes_funny',
+    'weighted_vote_score',
+    'comment_count',
+    'steam_purchase',
+    'received_for_free',
+    'written_during_early_access',
 ]
 
 # USERS RELATED APIs
 
 def resolve_vanity_url(vanity_url):
     """Get steam_id from accounts with custom url
-
     Args:
-
     Returns:
-
     """
 
     url = f"https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key={config.STEAM_API_KEY}&vanityurl={vanity_url}"
@@ -46,11 +73,8 @@ def resolve_vanity_url(vanity_url):
 
 def get_player_summaries(steam_id):
     """Get player summaries from steam_id
-    
     Args:
-
     Returns:
-
     """
 
     url = f"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={config.STEAM_API_KEY}&steamids={steam_id}"
@@ -66,10 +90,8 @@ def get_recently_played_games(steam_id):
 
 def get_app_details(app_id):
     """Get app details in English.
-
     Args:
         app_id (str): The game's id.
-
     Returns:
         dict: A Python dictionary containing the app details
               retrieved from the API.
@@ -79,9 +101,8 @@ def get_app_details(app_id):
     response = requests.get(url)
     return json.loads(response.text)
 
-def get_app_reviews(app_id):
+def get_app_reviews(app_id, cursor='*'):
     """Get app reviews
-
     Args:
         app_id (str): The game's id.
 
@@ -89,8 +110,8 @@ def get_app_reviews(app_id):
         dict: A Python dictionary containing the app reviews 
               retrieved from the API.
     """
-
-    url = f"https://store.steampowered.com/appreviews/{app_id}?json=1"
+    cursor = requests.utils.quote(cursor)
+    url = f"https://store.steampowered.com/appreviews/{app_id}?json=1&cursor={cursor}"
     response = requests.get(url)
     return json.loads(response.text)
 
@@ -112,13 +133,17 @@ def collect_games_data():
                     name = app_details['data']['name']
                     required_age = app_details['data']['required_age']
                     is_free = app_details['data']['is_free']
-                    categories = '|'.join(category['description'] for category in app_details['data']['categories'])
-                    genres = '|'.join(genre['description'] for genre in app_details['data']['genres'])
-                    short_description = app_details['data']['short_description']
                     developers = '|'.join(developer for developer in app_details['data']['developers'])
                     publishers = '|'.join(publisher for publisher in app_details['data']['publishers'])
+                    platforms_windows = app_details['data']['platforms']['windows']
+                    platforms_mac = app_details['data']['platforms']['mac']
+                    platforms_linux = app_details['data']['platforms']['linux']
+                    metacritic = app_details['data']['metacritic']['score'] if 'metacritic' in app_details['data'] else "N/A"
+                    categories = '|'.join(category['description'] for category in app_details['data']['categories'])
+                    genres = '|'.join(genre['description'] for genre in app_details['data']['genres'])
+                    recommendations = app_details['data']['recommendations']['total'] if 'recommendations' in app_details['data'] else "N/A"
+                    coming_soon = app_details['data']['release_date']['coming_soon']
                     release_date = app_details['data']['release_date']['date']
-                    metacritic_score = app_details['data']['metacritic']['score'] if 'metacritic' in app_details['data'] else 'N/A'
 
                     review_score = app_reviews['query_summary']['review_score']
                     review_score_desc = app_reviews['query_summary']['review_score_desc']
@@ -127,13 +152,29 @@ def collect_games_data():
                     total_reviews = app_reviews['query_summary']['total_reviews']
 
                     row = [
-                        app_id, name, required_age, is_free,
-                        categories, genres,
-                        short_description,
-                        developers, publishers,
+                        # Details
+                        app_id,
+                        name,
+                        required_age,
+                        is_free,
+                        developers,
+                        publishers,
+                        platforms_windows,
+                        platforms_mac,
+                        platforms_linux,
+                        metacritic,
+                        categories,
+                        genres,
+                        recommendations,
+                        coming_soon,
                         release_date,
-                        metacritic_score,
-                        review_score, review_score_desc, total_positive, total_negative, total_reviews
+
+                        # Reviews
+                        review_score,
+                        review_score_desc,
+                        total_positive,
+                        total_negative,
+                        total_reviews
                     ]
 
                     writer.writerow(row)
@@ -155,35 +196,63 @@ def collect_users_games_data():
 
                 print(f"Retrieving reviews for: {app_id}...", end=" ")
 
-                app_reviews = get_app_reviews(app_id)
+                cursor = '*'
+                for batch in range(1, 26):
+                    app_reviews = get_app_reviews(app_id, cursor)
+                    if app_reviews['success'] == True:
+                        if batch == 1:
+                            print("SUCCESS")
+                        print(f"Retrieving user data from reviews of {app_id} (batch: {batch})...", end=" ")
 
-                if app_reviews['success'] == True:
-                    print("SUCCESS")
-                    print(f"Retrieving user data from reviews of {app_id}...", end=" ")
+                        for review in app_reviews['reviews']:
+                            steam_id = review['author']['steamid']
+                            num_games_owned = review['author']['num_games_owned']
+                            num_reviews = review['author']['num_reviews']
+                            playtime_forever = review['author']['playtime_forever']
+                            playtime_last_two_weeks = review['author']['playtime_last_two_weeks']
+                            playtime_at_review = review['author']['playtime_at_review']
+                            last_played = review['author']['last_played']
+                            timestamp_created = review['timestamp_created']
+                            timestamp_updated = review['timestamp_updated']
+                            voted_up = review['voted_up']
+                            votes_up = review['votes_up']
+                            votes_funny = review['votes_funny']
+                            weighted_vote_score = review['weighted_vote_score']
+                            comment_count = review['comment_count']
+                            steam_purchase = review['steam_purchase']
+                            received_for_free = review['received_for_free']
+                            written_during_early_access = review['written_during_early_access']
 
-                    for review in app_reviews['reviews']:
-                        steam_id = review['author']['steamid']
-                        voted_up = review['voted_up']
-                        votes_up = review['votes_up']
-                        votes_funny = review['votes_funny']
-                        playtime_forever = review['author']['playtime_forever']
-                        playtime_last_two_weeks = review['author']['playtime_last_two_weeks']
-                        playtime_at_review = review['author']['playtime_at_review']
-                        timestamp_created = review['timestamp_created']
-                        timestamp_updated = review['timestamp_updated']
+                            row = [
+                                steam_id,
+                                app_id,
+                                num_games_owned,
+                                num_reviews,
+                                playtime_forever,
+                                playtime_last_two_weeks,
+                                playtime_at_review,
+                                last_played,
+                                timestamp_created,
+                                timestamp_updated,
+                                voted_up,
+                                votes_up,
+                                votes_funny,
+                                weighted_vote_score,
+                                comment_count,
+                                steam_purchase,
+                                received_for_free,
+                                written_during_early_access,
+                            ]
 
-                        row = [
-                            steam_id, app_id,
-                            voted_up, votes_up, votes_funny,
-                            playtime_forever, playtime_last_two_weeks, playtime_at_review,
-                            timestamp_created, timestamp_updated
-                        ]
+                            writer.writerow(row)
 
-                        writer.writerow(row)
-
-                    print("DONE")
-                else:
-                    print("FAILURE")
+                        print("DONE")
+                        cursor = app_reviews['cursor']
+                        if cursor == '*':
+                            print(f"Last batch of reviews for {app_id} reached.")
+                            break
+                    else:
+                        print("FAILURE")
 
     print("Sorting by steam_id...", end=" ")
     utils.read_and_sort_csv(users_games_csv_path, 0)
