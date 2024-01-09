0127b68d70d0dcdd1415ee7c3dcdcb51c7463078
Mai Anh Quân <108073174+qvan7um@users.noreply.github.com>
2024-01-09 11:18:07 +0700

    specify the correct encoding

--- common/utils.py
+++ common/utils.py
@@ -1,16 +1,19 @@
 import csv
 
-def read_and_sort_csv(file_name, column_index):
-    with open(file_name, 'r') as csv_file:
+def read_and_sort_csv(file_name, column_index, as_number=False):
+    with open(file_name, 'r', newline='', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
         header = next(reader)
         data = list(reader)
         
     # Sort the data based on the 'steam_id' column (assuming it's the first column)
-    data.sort(key=lambda row: row[column_index])
+    if as_number:
+        data.sort(key=lambda row: int(row[column_index]))
+    else:
+        data.sort(key=lambda row: row[column_index])
     
     # Write the sorted data back into the CSV file
-    with open(file_name, 'w', newline='') as csv_file:
+    with open(file_name, 'w', newline='', encoding='utf-8') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(header)
         writer.writerows(data)
